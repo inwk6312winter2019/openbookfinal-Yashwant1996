d875b30b346799779ca0c9f8d63ca8954c140eab
Yashwant1996 <ys621290@dal.ca>
2019-02-15 14:18:11 -0400

    total words

---
+++ task1A2.py
@@ -0,0 +1,11 @@
+def count_the_article():
+ file=open("Book1.txt","r+")
+ wordcount={}
+ for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+ print (word,wordcount)
+ file.close();
+count_the_article()
