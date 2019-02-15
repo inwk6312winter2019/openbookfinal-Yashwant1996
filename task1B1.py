6b9468edf2f7c070f8da4f8a7272bb1781249ae7
Yashwant1996 <ys621290@dal.ca>
2019-02-15 14:51:50 -0400

    halfway

--- task1B1.py
+++ task1B1.py
@@ -3,5 +3,13 @@ import collections
 
 def top_20_common_words(textfile, top=10):    
     """  text file IS book1 here """
+  textfile = open(textfile)
+    text = textfile.read().lower()
+    textfile.close()
+    words = collections.Counter(text.split()) # how often each word appears
 
-    
+    return dict(words.most_common(top))
+
+filename = sys.argv[0]
+top_20_common_words('Book1.txt')
+
